46f8bd3bc8887dd7f6614f4e1884e2e2e1c783d5
Andrewpqc <m13636038496@163.com>
2018-05-25 17:25:33 +0800

    add some basic constants and macros

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -26,9 +26,9 @@ team_t team = {
     /* Team name */
     "ateam",
     /* First member's full name */
-    "Harry Bovik",
+    "Andrewpqc",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "andrewpqc@mails.ccnu.edu.cn",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
@@ -44,11 +44,51 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define WSIZE 4
+
+#define DSIZE 8
+
+#define CHUNKSIZE (1<<12)
+
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
+
+#define PACK(size,alloc) ((size)|(alloc))
+
+#define GET(p) (*(unsigned int*)(p))
+
+#define PUT(p,val) (*(unsigned int*)(p)=(val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(bp) ((char*)(bp)-WSIZE)
+
+#define FTRP(bp) ((char*)(bp)+GET_SIZE(HDRP(bp))-DSIZE)
+
+#define NEXT_BLKP(bp) ((char*)(bp)+GET_SIZE(((char*)(bp)-WSIZE)))
+
+#define PREV_BLKP(bp) ((char*)(bp)-GET_SIZE(((char*)(bp)-DSIZE)))
+
+//static function statement
+static void *extend_heap(size_t words){
+    
+}
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    if((heap_listp=mem_sbrk(4*WSIZE))==NULL)
+        return -1;
+    PUT(heap_listp,0);
+    PUT(heap_listp+(1*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listp+(2*WSIZE),PACK(DSIZE,1));
+    PUT(heap_listp+(3*WSIZE),PACK(0,1));
+    heap_listp+=(2*WSIZE);
+    if(extend_heap(CHUNKSIZE/WSIZE)==NULL)
+        return -1;
     return 0;
 }
 
@@ -61,7 +101,7 @@ void *mm_malloc(size_t size)
     int newsize = ALIGN(size + SIZE_T_SIZE);
     void *p = mem_sbrk(newsize);
     if (p == (void *)-1)
-	return NULL;
+	    return NULL;
     else {
         *(size_t *)p = size;
         return (void *)((char *)p + SIZE_T_SIZE);
